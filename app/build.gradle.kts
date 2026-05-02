e84b5c9c249050f316c967e84df8b0e1a1c74aed
jh-akt <jh-akt@users.noreply.github.com>
2026-05-02 20:54:06 +0800

    Release 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -168,8 +168,8 @@ android {
         applicationId = "com.maaframework.android.gf2"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
